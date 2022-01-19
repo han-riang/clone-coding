de0fd5ee9697e40fb110dc185b10e275ea916599
han-riang <dy0358@daum.net>
2022-01-19 15:26:29 +0900

    D-day program update
    D-day program is made by myself from example of Christmas Calculator.

---
+++ momentum/D-day program/D-day program/src/index.js
@@ -0,0 +1,29 @@
+const clockTitle = document.querySelector(".js-clock");
+
+const date = new Date();
+const eveDate = new Date("December 24, 2022 00:00:00");
+const nowHour = date.getHours();
+const nowMin = date.getMinutes();
+const nowSec = date.getSeconds();
+
+function dayCalculaor() {
+  const eveTime = eveDate.getTime();
+  const nowTime = Date.now();
+  const dDay = Math.floor((eveTime - nowTime) / 86400000);
+  return dDay;
+}
+toEveAlarm();
+function toEveAlarm() {
+  const toEveDay = dayCalculaor();
+  const date = new Date();
+  const nowHour = date.getHours();
+  const nowMin = date.getMinutes();
+  const nowSec = date.getSeconds();
+  const toEveHour = 23 - nowHour;
+  const toEveMin = 59 - nowMin;
+  const toEveSec = 59 - nowSec;
+
+  clockTitle.innerText = `${toEveDay}d ${toEveHour}h ${toEveMin}m ${toEveSec}s`;
+}
+
+setInterval(toEveAlarm, 1000);
