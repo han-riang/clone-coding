8f90feb5b2f63963e523627681084de1b4b5a94b
han-riang <dy0358@daum.net>
2022-01-19 15:34:35 +0900

    D-day clone-change position
    D-day clone-change position

--- momentum/D-day program/D-day program/src/index.js
+++ momentum/D-day program/D-day program/src/index.js
@@ -14,12 +14,14 @@ function dayCalculaor() {
 }
 toEveAlarm();
 function toEveAlarm() {
-    const toEveDay = dayCalculaor();
     const date = new Date();
     const nowHour = date.getHours();
     const nowMin = date.getMinutes();
     const nowSec = date.getSeconds();
-  const toEveHour = 23 - nowHour;
+  
+  
+    const toEveDay = dayCalculaor();
+    const toEveHour = 23 - nowHour;
   const toEveMin = 59 - nowMin;
   const toEveSec = 59 - nowSec;
 
